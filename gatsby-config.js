5100b232966b47399af9309209f191c592fb2027
pia smith <basiccreation@yahoo.com>
2019-04-04 22:55:17 -0700

    added react icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,8 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
-    },{
+    },
+    {
       resolve: `gatsby-plugin-styled-components`,
       options: {
         // Add any options here
