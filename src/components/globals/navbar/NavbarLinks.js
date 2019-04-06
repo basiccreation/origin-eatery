f90091b64cb6c41c7658a9f31444645c0fad185f
pia smith <basiccreation@yahoo.com>
2019-04-06 12:39:49 -0700

    added navigation items to the nav

--- src/components/globals/navbar/NavbarLinks.js
+++ src/components/globals/navbar/NavbarLinks.js
@@ -1,7 +1,49 @@
 import React, { Component } from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
+import { styles } from "../../../utils"
+
 export default class NavbarLinks extends Component {
+  state = {
+    links: [
+      {
+        id: 0,
+        path: "/",
+        name: "home",
+      },
+      {
+        id: 1,
+        path: "/about/",
+        name: "about",
+      },
+      {
+        id: 2,
+        path: "/menu/",
+        name: "menu",
+      },
+      {
+        id: 3,
+        path: "/contact/",
+        name: "contact",
+      },
+    ],
+  }
+
   render() {
-    return <div>content from nav bar links</div>
+    return (
+      <LinkWrapper open={this.props.navbarOpen}>
+        {this.state.links.map(item => {
+          return (
+            <li key={item.id}>
+              <Link to={item.path} className="nav-link">
+                {item.name}
+              </Link>
+            </li>
+          )
+        })}
+      </LinkWrapper>
+    )
   }
 }
+
+const LinkWrapper = styled.ul``
