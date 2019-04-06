65084873f11c2d3e70cd42f377c8c62387cdaad7
pia smith <basiccreation@yahoo.com>
2019-04-06 12:21:10 -0700

    added styles.js in utils folder

--- src/components/globals/navbar/NavbarHeader.js
+++ src/components/globals/navbar/NavbarHeader.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 import logo from "../../../images/logo.svg"
 import { FaAlignRight } from "react-icons/fa"
 import styled from "styled-components"
-
+import { styles } from "../../../utils"
 export default class NavbarHeader extends Component {
   render() {
     const { handleNavbar } = this.props
@@ -12,10 +12,28 @@ export default class NavbarHeader extends Component {
         <Link to="/">
           <img src={logo} alt="Origin Eatery Logo" />
         </Link>
-        content from HeaderWrapper
+        <FaAlignRight
+          className="toggle-icon"
+          onClick={() => {
+            handleNavbar()
+          }}
+        />
       </HeaderWrapper>
     )
   }
 }
-
-const HeaderWrapper = `styled.div`
+const red = "red"
+const HeaderWrapper = styled.div`
+  padding: 0.4rem 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .toggle-icon {
+    font-size: 1.75rem;
+    color: ${styles.colors.mainYellow};
+    cursor: pointer;
+  }
+  @media (min-width: 768px) {
+    display: none;
+  }
+`
