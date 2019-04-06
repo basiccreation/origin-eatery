fa02413390060ab0154abbcf1d9411b437191b72
pia smith <basiccreation@yahoo.com>
2019-04-06 09:04:33 -0700

    added logo to Navbar

--- src/components/globals/navbar/NavbarHeader.js
+++ src/components/globals/navbar/NavbarHeader.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react"
 import { Link } from "gatsby"
 import logo from "../../../images/logo.svg"
+import { FaAlignRight } from "react-icons/fa"
+import styled from "styled-components"
 
 export default class NavbarHeader extends Component {
   render() {
-    return <div>content from navbar header</div>
+    const { handleNavbar } = this.props
+    return (
+      <HeaderWrapper>
+        <Link to="/">
+          <img src={logo} alt="Origin Eatery Logo" />
+        </Link>
+        content from HeaderWrapper
+      </HeaderWrapper>
+    )
   }
 }
+
+const HeaderWrapper = `styled.div`
