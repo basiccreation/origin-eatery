f5a02196e34de079ff7f91ee3f3415c754beb36d
pia smith <basiccreation@yahoo.com>
2019-04-06 00:45:41 -0700

    added responsive nav feature

--- src/components/globals/navbar/Navbar.js
+++ src/components/globals/navbar/Navbar.js
@@ -2,15 +2,30 @@ import React, { Component } from "react"
 import NavbarHeader from "./NavbarHeader"
 import NavbarIcons from "./NavbarIcons"
 import NavbarLinks from "./NavbarLinks"
-
+import styled from "styled-components"
 export default class Navbar extends Component {
+  state = {
+    navbarOpen: false,
+  }
+  handleNavbar = () => {
+    this.setState(() => {
+      return { navbarOpen: !this.state.navbarOpen }
+    })
+  }
   render() {
     return (
-      <nav>
-        <NavbarHeader />
-        <NavbarLinks />
+      <NavWrapper>
+        <NavbarHeader handleNavbar={this.handleNavbar} />
+        <NavbarLinks navbarOpen={this.state.navbarOpen} />
         <NavbarIcons />
-      </nav>
+      </NavWrapper>
     )
   }
 }
+
+const NavWrapper = styled.nav`
+  @media (min-width: 760px) {
+    display: flex;
+    align-items: center;
+  }
+`
