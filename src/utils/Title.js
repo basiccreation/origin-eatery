52d9ae782aa56fadabd8d3d9026887fa8300967e
pia smith <basiccreation@yahoo.com>
2019-04-06 23:27:34 -0700

    added title component

--- src/utils/Title.js
+++ src/utils/Title.js
@@ -1,12 +1,37 @@
 import React from "react"
 import styled from "styled-components"
-
+import { styles } from "."
 export function Title({ title, message }) {
   return (
-    <div>
+    <TitleWrapper>
       <h3>{message}</h3>
       <h1>{title}</h1>
       <div className="underline" />
-    </div>
+    </TitleWrapper>
   )
 }
+Title.defaultProps = {
+  message: "our message",
+  title: "our title",
+}
+
+const TitleWrapper = styled.div`
+  text-align: center;
+  h3 {
+    ${styles.textSlanted};
+    ${styles.letterSpacing({ spacing: "0.3rem" })};
+    font-size: 2rem;
+    color: ${styles.colors.mainYellow};
+  }
+  h1 {
+    ${styles.letterSpacing({ spacing: "0.3rem" })};
+    font-size: 2rem;
+    text-transform: uppercase;
+  }
+  .underline {
+    width: 5rem;
+    height: 0.2rem;
+    background: ${styles.colors.mainYellow};
+    margin: 0.5rem auto;
+  }
+`
