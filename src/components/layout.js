97c424ea4f920bd869d9772a50a0c7200edace63
pia smith <basiccreation@yahoo.com>
2019-04-07 21:49:25 -0700

    tweaking footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { createGlobalStyle } from "styled-components"
 import Navbar from "../components/globals/navbar"
-import Footer from "../components/globals/Footer"
+import Footer from "../components/globals/footer"
 const Layout = ({ children }) => {
   return (
     <React.Fragment>
